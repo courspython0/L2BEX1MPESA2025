a1d59bbec470822240dcb62edc7ce0eb7d8a3b4f
kevinlebughe488-pixel <kevinlebughe488@gmail.com>
2025-09-18 23:23:43 +0100

    Ajout du dossier TP_PYTHON_LEBUGHE_YOKA avec les fichiers du TP

---
+++ TP_PYTHON_LEBUGHE_YOKA/MEMBRES.py
@@ -0,0 +1,9 @@
+#  ==== Membres du groupe ====
+#  ====  _________________
+
+#  ====  LEBUGHE YOKA KEVIN           ETUL2PY001
+#  ====  NTUMBA TSHILOBO SAMUEL       ETUL2PY024
+#  ====  BINTU LUBILANJI LAURIANNE    ETUL2PY031
+#  ====  BIYOKO NTIKUBULA BIDA        ETUL2PY037
+#  ====  KOBI KIFUA PATRICIA
+#  ====  NGOYI KITENGE BEATRICE
